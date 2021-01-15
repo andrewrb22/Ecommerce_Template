c12e2eb9d9975256348a7fee6ab261e9ac273cff
Andrew Rodriguez <andrewrb22@gmail.com>
2021-01-14 22:21:49 -0500

    updated user action for user info

--- DoArt/doart/src/actions/userActions.js
+++ DoArt/doart/src/actions/userActions.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 import Cookie from 'js-cookie';
-import { USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS } from '../constants/userConstants.js';
+import {USER_DETAILS_SUCCESS, USER_DETAILS_FAIL,USER_DETAILS_REQUEST, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS } from '../constants/userConstants.js';
 
 
 const signin = (email, password) => async (dispatch) => {
@@ -26,7 +26,21 @@ const register = (name, email, password) => async (dispatch) => {
   }
   
   const detailsUser = (userId) => async(dispatch, getState) =>{
-    
+    dispatch({ type:USER_DETAILS_REQUEST, payload: userId});
+    const {userSignin: {userInfo}} =getState();
+try {
+  const {data} = Axios.get(`/api/users/ ${userId}`,{
+    headers: {Authorization: `Bearer ${userInfo.token}`}
+  });
+  dispatch({type: USER_DETAILS_SUCCESS, payload: data})
+} catch (error) {
+  const message = error.response && error.response.data.message
+  ? error.response.data.message: error.message;
+  dispatch({type: USER_DETAILS_FAIL, payload: message})
+  
+}
+
+
   }
 
 export { signin, register };
--- DoArt/doart/src/reducers/userReducer.js
+++ DoArt/doart/src/reducers/userReducer.js
@@ -22,6 +22,17 @@ function userRegisterReducer(state = {}, action) {
       default: return state;
     }
   };
+  function userRegisterReducer(state = {}, action) {
+    switch (action.type) {
+      case USER_REGISTER_REQUEST:
+        return { loading: true };
+      case USER_REGISTER_SUCCESS:
+        return { loading: false, userInfo: action.payload };
+      case USER_REGISTER_FAIL:
+        return { loading: false, error: action.payload };
+      default: return state;
+    }
+  };
 
 export {
     userSigninReducer, userRegisterReducer
