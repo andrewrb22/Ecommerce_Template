a263f6b37848ee228eaf4c83a957919f1633bfd7
Andrew Rodriguez <andrewrb22@gmail.com>
2021-01-19 22:46:32 -0500

    updated css for payment screen

--- DoArt/doart/src/Screens/PaymentScreen.js
+++ DoArt/doart/src/Screens/PaymentScreen.js
@@ -1,54 +1,80 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { savePayment } from '../actions/cartActions'
 import CheckoutSteps from '../components/CheckoutSteps';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn,MDBIcon} from 'mdbreact'
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from 'mdbreact'
 
 
 function PaymentScreen(props) {
 
-    const [paymentMethod, setPaymentMethod] = useState('');
+  const [paymentMethod, setPaymentMethod] = useState('');
 
-   
-    const dispatch = useDispatch();
-  
-   
-   
-  
-    const submitHandler = (e) => {
-      e.preventDefault();
-      dispatch(savePayment({paymentMethod}));
-      props.history.push('placeorder')
-    }
-    return <div>
-        <CheckoutSteps step1 step2 step3></CheckoutSteps>
-        <MDBContainer>
-                    <MDBRow>
-                        <MDBCol md="12">
-                            <div className="form">
-                               <form onSubmit={submitHandler} >
-                                    <p className="h2 text-center mb-4">Payment Option</p>
-                                     <div>   
-            
-                                        <input  type="radio" name="paymentMethod" id="paymentMethod" value="paypal" onChange={(e) => setPaymentMethod(e.target.value)}>
+
+  const dispatch = useDispatch();
+
+
+
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePayment({ paymentMethod }));
+    props.history.push('placeorder')
+  }
+  return <div>
+    <CheckoutSteps step1 step2 step3></CheckoutSteps>
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol md="12">
+          <div className="form">
+            <form onSubmit={submitHandler} >
+              <p className="h2 text-center mb-4">Payment Option</p>
+              <div className="text-center">
+
+                <input type="radio" name="paymentMethod" id="paymentMethod" value="paypal" onChange={(e) => setPaymentMethod(e.target.value)}>
+                </input>
+                <label htmlFor="paymentMethod" className="black-text  mb-4 h4">
+
+                  <MDBIcon fab icon="cc-paypal" size="5x" />
+
+
+                </label>
+                
+                <input  type="radio" name="paymentMethod" id="paymentMethod" value="paypal" onChange={(e) => setPaymentMethod(e.target.value)}>
                                             </input>
                                                  <label  htmlFor="paymentMethod" className="black-text  mb-4 h4">
                                                
-                                                 <MDBIcon fab icon="cc-paypal" size="6x" />
+                                                 <MDBIcon fab icon="cc-apple-pay" size="5x" />
                                                  
             
-       </label>
-                                      </div>
-          
-                                        <MDBBtn color="primary" type="submit" className="button primary">
-                                                      Continue
-                                          </MDBBtn>
-                                 </form>
-                              </div>
-                          </MDBCol>
-                      </MDBRow>
-                  </MDBContainer>
+                                                  </label>
 
-    </div>
-  }
-  export default  PaymentScreen;
+                <input type="radio" name="paymentMethod" id="paymentMethod" value="paypal" onChange={(e) => setPaymentMethod(e.target.value)}>
+                </input>
+                <label htmlFor="paymentMethod" className="black-text  mb-4 h4">
+
+                <MDBIcon fab icon="cc-visa"size="5x" />
+
+
+                </label>
+                <input type="radio" name="paymentMethod" id="paymentMethod" value="paypal" onChange={(e) => setPaymentMethod(e.target.value)}>
+                </input>
+                <label htmlFor="paymentMethod" className="black-text  mb-4 h4">
+
+                <MDBIcon fab icon="cc-mastercard"size="5x" />
+
+
+                </label>
+              </div>
+
+              <MDBBtn  color="primary" type="submit" className="button primary ">
+                Continue
+                                          </MDBBtn>
+            </form>
+          </div>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+
+  </div>
+}
+export default PaymentScreen;
