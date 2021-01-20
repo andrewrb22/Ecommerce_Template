1ecbb5b3b3df3dd7ae1eb21164a048528cbde611
Andrew Rodriguez <andrewrb22@gmail.com>
2021-01-19 21:55:38 -0500

    updated shipping form screen

--- DoArt/doart/src/Screens/ShippingScreen.js
+++ DoArt/doart/src/Screens/ShippingScreen.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { saveShipping } from '../actions/cartActions'
 import CheckoutSteps from '../components/CheckoutSteps';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from 'mdbreact';
 
 
 function ShippingScreen(props) {
 
   const [address, setAddress] = useState('');
   const [city, setCity] = useState('');
-  const [country, setCountry] = useState('');
+  const [state, setstate] = useState('');
   const [postal, setPostal] = useState('');
 
   const dispatch = useDispatch();
@@ -18,7 +19,7 @@ function ShippingScreen(props) {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    dispatch(saveShipping({ address, city, postal, country }));
+    dispatch(saveShipping({ address, city, postal, state }));
     props.history.push('payment')
   }
   return <div>
@@ -26,48 +27,24 @@ function ShippingScreen(props) {
 
     <div className="form">
       <form onSubmit={submitHandler} >
-        <ul className="form-container">
-          <li>
-            <h2>Shipping </h2>
-          </li>
-
-          <li>
-            <label htmlFor="address">
-              Address
-            </label>
-            <input type="text" name="address" id="address" onChange={(e) => setAddress(e.target.value)}>
-            </input>
-          </li>
-
-          <li>
-            <label htmlFor="city">
-              City
-            </label>
-            <input type="text" name="city" id="city" onChange={(e) => setCity(e.target.value)}>
-            </input>
-          </li>
-
-          <li>
-            <label htmlFor="country">
-              State
-            </label>
-            <input type="text" name="country" id="country" onChange={(e) => setCountry(e.target.value)}>
-            </input>
-          </li>
-          <li>
-            <label htmlFor="postal">
-              Postal Code
-            </label>
-            <input type="text" name="postal" id="postal" onChange={(e) => setPostal(e.target.value)}>
-            </input>
-          </li>
+        <p className="h2 text-center mb-4">Shipping</p>
+        <div className="black-text">
+          <MDBInput label="Address" icon="user" type="text" name="address" id="address" onChange={(e) => setAddress(e.target.value)}
+            success="right" />
+          <MDBInput label="City" icon="dollar-sign" name="city" id="city" onChange={(e) => setCity(e.target.value)}
+            success="right" />
+          <MDBInput label="State" icon="image" type="text" name="state" id="state" onChange={(e) => setstate(e.target.value)} success="right" />
+          <MDBInput label="Postal" icon="info-circle" type="text" name="postal" id="postal" onChange={(e) => setPostal(e.target.value)} />
 
 
-          <li>
-            <button type="submit" className="button primary">Continue</button>
-          </li>
 
-        </ul>
+
+
+          <div>
+            <MDBBtn color="primary" type="submit" className="button primary">Continue</MDBBtn>
+          </div>
+
+        </div>
       </form>
     </div>
   </div>
