84ba6b219709c0a689f59c1061624ea626ff36fe
Andrew Rodriguez <andrewrb22@gmail.com>
2021-01-23 19:06:19 -0500

    code

--- DoArt/backend/server.js
+++ DoArt/backend/server.js
@@ -51,10 +51,10 @@ app.get('/api/config/paypal', (req, res) => {
 // })
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/../DoArt/doart/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../DoArt/doart/public/index.html`));
-});
+app.use(express.static(path.join(__dirname, '/DoArt/doart/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/DoArt/doart/build/index.html'))
+);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
